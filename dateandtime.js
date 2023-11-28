f24c82bb6d298b9b4085a770196d20eaf6550e3e
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-11-28 09:07:56 +0000

    time and date

---
+++ dateandtime.js
@@ -0,0 +1,15 @@
+const now = new Date();
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDay());
+
+
+console.log(now.getHours());
+console.log(now.getMilliseconds());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toLocaleString());
+console.log(now.toLocalTimeString());
